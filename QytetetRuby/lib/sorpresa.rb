94055f32b0afb95a2d21baeea444b467f684a8ff
jmplz14 <jmplz14@correo.ugr.es>
2017-09-29 13:29:02 +0200

    Creado el enum

--- QytetetRuby/lib/sorpresa.rb
+++ QytetetRuby/lib/sorpresa.rb
@@ -1,6 +1,13 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+
 module ModeloQytetet
-  puts "Hello World"
+  class Sorpresa
+    attr_reader :texto, :valor, :tipo
+    
+    def initialize
+        
+    end
+  end
 end
--- QytetetRuby/lib/tipo_sopresa.rb
+++ QytetetRuby/lib/tipo_sopresa.rb
@@ -1,9 +1,15 @@
-# To change this license header, choose License Headers in Project Properties.
+#encoding utf-8
+## To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
 module ModeloQytetet
   module TipoSopresa
+      PAGARCOBAR = :PagarCobrar
+      IRACASILLA = :IrACasilla
+      PORCASAHOTEL = :PorCasaHotel
+      PORJUGADOR = :PorJugador
+      SALIRARCEL = :SalirCárcel
       
   end
 end
