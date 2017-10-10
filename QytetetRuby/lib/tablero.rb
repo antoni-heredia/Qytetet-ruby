168ab4de47e40aeb417ef364f57d83a7c259a040
jmplz14 <jmplz14@correo.ugr.es>
2017-10-10 22:08:01 +0200

    Elimino los require_relative innecesarios.

--- QytetetRuby/lib/tablero.rb
+++ QytetetRuby/lib/tablero.rb
@@ -1,9 +1,6 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
-require_relative "casilla"
-require_relative "tipo_casilla"
-require_relative "titulo_propiedad"
 
 module ModeloQytetet
   class Tablero
