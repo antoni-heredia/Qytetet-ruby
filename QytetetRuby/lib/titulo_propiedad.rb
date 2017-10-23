a32047bdc33ccdba53049471d115e20bfbc7fa92
jmplz14 <jmplz14@correo.ugr.es>
2017-10-23 21:10:03 +0200

    commit de la clase tituloPropiedad

--- QytetetRuby/lib/titulo_propiedad.rb
+++ QytetetRuby/lib/titulo_propiedad.rb
@@ -6,6 +6,7 @@ module ModeloQytetet
   class TituloPropiedad
     attr_reader :nombre, :alquiler_base, :factor_revalorizacion, :hipoteca_base, :precio_edificar
     attr_accessor :hipotecada
+    attr_writer :casilla, :propietario
     
     #Constructor 
     def initialize (nombre,hipotecada, alquiler_base, factor_revalorizacion, hipoteca_base, precio_edificar)
@@ -15,6 +16,8 @@ module ModeloQytetet
       @factor_revalorizacion = factor_revalorizacion
       @hipoteca_base = hipoteca_base
       @precio_edificar = precio_edificar
+      @casilla
+      @propietario
     end
     
     #Constructor sin necesidad de decir si esta hipotecada, ya que por defecto sera false
@@ -26,6 +29,22 @@ module ModeloQytetet
     def self.crear_todo_defecto(nombre)
       self.crear(nombre,50, 0.1, 150, 250)
     end
+=begin
+    def cobrar_alquiler(coste)
+      
+    end
+
+    def propietario_encarcelado()
+      
+    end
+
+    def tengo_propietario()
+      
+    end
+=end
+    
+    
+    
     
     #Metodo que devuelve una cadena de caracteres con la informacion del titulo de la propiead
     def to_s
