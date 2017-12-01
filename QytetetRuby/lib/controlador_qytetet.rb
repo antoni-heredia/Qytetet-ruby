36dc788f3fb25b65b6a15048ffd25d03db0267d7
Antonio Heredia <a.heredia.castillo@gmail.com>
2017-12-01 01:24:29 +0100

    El controlador funciona de manera normal

--- QytetetRuby/lib/controlador_qytetet.rb
+++ QytetetRuby/lib/controlador_qytetet.rb
@@ -8,187 +8,189 @@ require_relative 'tipo_casilla'
 
 module InterfazTextualQytetet
   class Controlador_qytetet
-    @@vista = VistaTextualQytetet.new
-    @@juego = ModeloQytetet::Qytetet.instance;
     
-
-    def self.incializar_juego()
+    
+    def initialize
+      @vista = VistaTextualQytetet.new
+      @juego = ModeloQytetet::Qytetet.instance;
+    end
+    def incializar_juego()
         
       nombres = Array.new
-      nombres = @@vista.obtener_nombre_jugadores
+      nombres = @vista.obtener_nombre_jugadores
       #nombres << "Juan"
       #nombres << "Pepe"
-      @@juego.inicializar_juego(nombres)
-      @@jugador = @@juego.jugador_actual
-      @@casilla = @@jugador.casilla_actual
-      @@vista.mostrar(@@juego.to_s)
-      @@vista.pausa
+      @juego.inicializar_juego(nombres)
+      @jugador = @juego.jugador_actual
+      @casilla = @jugador.casilla_actual
+      @vista.mostrar(@juego.to_s)
+      @vista.pausa
     end
-    def self.gestion_inmobiliarioa(opcion)
+    def gestion_inmobiliarioa(opcion)
       casillas = Array.new
       case opcion
 
       when 1
-        @@vista.mostrar("Todas tus propiedades: \n")
-        casillas = @@juego.propiedades_hipotecadas_jugador(true)
-        casillas += @@juego.propiedades_hipotecadas_jugador(false)
+        @vista.mostrar("Todas tus propiedades: \n")
+        casillas = @juego.propiedades_hipotecadas_jugador(true)
+        casillas += @juego.propiedades_hipotecadas_jugador(false)
         casilla_elegida = elegir_propiedad(casillas)
-        if(@@juego.edificar_casa(casilla_elegida))
-          @@vista.mostrar("Se ha edificado una casa")
+        if(@juego.edificar_casa(casilla_elegida))
+          @vista.mostrar("Se ha edificado una casa")
         else
-          @@vista.mostrar("No se ha podido edificar casa")
+          @vista.mostrar("No se ha podido edificar casa")
         end
       when 2
-        @@vista.mostrar("Todas tus propiedades: \n")
-        casillas = @@juego.propiedades_hipotecadas_jugador(true)
-        casillas += @@juego.propiedades_hipotecadas_jugador(false)
+        @vista.mostrar("Todas tus propiedades: \n")
+        casillas = @juego.propiedades_hipotecadas_jugador(true)
+        casillas += @juego.propiedades_hipotecadas_jugador(false)
         casilla_elegida = elegir_propiedad(casillas)
-        if(@@juego.edificar_hotel(casilla_elegida))
-          @@vista.mostrar("Se ha edificado un hotel")
+        if(@juego.edificar_hotel(casilla_elegida))
+          @vista.mostrar("Se ha edificado un hotel")
         else
-          @@vista.mostrar("No se ha podido edificar hotel")
+          @vista.mostrar("No se ha podido edificar hotel")
         end
       when 3
-        casillas = @@juego.propiedades_hipotecadas_jugador(false)
+        casillas = @juego.propiedades_hipotecadas_jugador(false)
         if (casillas.any?)
-          @@vista.mostrar("Tus propiedades no hipotecadas: \n")
+          @vista.mostrar("Tus propiedades no hipotecadas: \n")
           casilla_elegida = elegir_propiedad(casillas)
-          @@juego.vender_propiedad(casilla_elegida)
+          @juego.vender_propiedad(casilla_elegida)
         else
-          @@vista.mostrar("No tiene propiedades para realizar esta opicon \n")
+          @vista.mostrar("No tiene propiedades para realizar esta opicon \n")
         end
 
       when 4
-        casillas = @@juego.propiedades_hipotecadas_jugador(false)
+        casillas = @juego.propiedades_hipotecadas_jugador(false)
         if (casillas.any?)
-          @@vista.mostrar("Tus propiedades no hipotecadas: \n")
+          @vista.mostrar("Tus propiedades no hipotecadas: \n")
           casilla_elegida = elegir_propiedad(casillas)
-          @@juego.hipotecar_propiedad(casilla_elegida)
+          @juego.hipotecar_propiedad(casilla_elegida)
         else
-          @@vista.mostrar("No tiene propiedades para realizar esta opicon \n")
+          @vista.mostrar("No tiene propiedades para realizar esta opicon \n")
         end
       when 5
-        casillas = @@juego.propiedades_hipotecadas_jugador(true)
+        casillas = @juego.propiedades_hipotecadas_jugador(true)
         if (casillas.any?)
-          @@vista.mostrar("Tus propiedades no hipotecadas: \n")
+          @vista.mostrar("Tus propiedades no hipotecadas: \n")
           casilla_elegida = elegir_propiedad(casillas)
-          @@juego.cancelar_hipoteca(casilla_elegida)
+          @juego.cancelar_hipoteca(casilla_elegida)
         else
-          @@vista.mostrar("No tiene propiedades para realizar esta opicon \n")
+          @vista.mostrar("No tiene propiedades para realizar esta opicon \n")
         end
       end
-      @@vista.mostrar("El saldo actual del jugador" + @@jugador.saldo.to_s)
+      @vista.mostrar("El saldo actual del jugador" + @jugador.saldo.to_s)
     end
     
-    def self.casilla_tipo_calle(no_tiene_propietario)
-      @@vista.mostrar("Casilla actual" + @@casilla.to_s)
+    def casilla_tipo_calle(no_tiene_propietario)
+      @vista.mostrar("Casilla actual" + @casilla.to_s)
       
       if(!no_tiene_propietario)
-        @@vista.mostrar("La casilla no tiene propietario")
-        comprar = @@vista.elegir_quiero_comprar
+        @vista.mostrar("La casilla no tiene propietario")
+        comprar = @vista.elegir_quiero_comprar
         if(comprar)
-          if(@@juego.comprar_titulo_propiedad)
-            @@vista.mostrar("Se ha comprado la propiedad su saldo actual es " + 
-              @@jugador.saldo.to_s);
+          if(@juego.comprar_titulo_propiedad)
+            @vista.mostrar("Se ha comprado la propiedad su saldo actual es " + 
+              @jugador.saldo.to_s);
             
           else
-            @@vista.mostrar("No se ha podido comprar la propiedad");                        
+            @vista.mostrar("No se ha podido comprar la propiedad");                        
           end
         end
       else
-        @@vista.mostrar("La casilla actual tiene propietario, tiene que pagar " + @@casilla.precio_alquiler.to_s + "\nSu saldo actual es de " + @@jugador.saldo.to_s)
+        @vista.mostrar("La casilla actual tiene propietario, tiene que pagar " + @casilla.precio_alquiler.to_s + "\nSu saldo actual es de " + @jugador.saldo.to_s)
       end
     end
     
-    def self.casilla_tipo_sorpresa(no_tiene_propietario)
-      @@vista.mostrar('Ha caido en la carta sorpresa\n')
-      @@vista.mostrar("La carte que le ha tocado es: " + @@juego.carta_actual.to_s() + "\n")
-      no_tiene_propietario = @@juego.aplicar_sorpresa
-      @@casilla = @@jugador.casilla_actual
-      if( !@@jugador.encarcelado && !bancarrota &&@@casilla.tipo == ModeloQytetet::TipoCasilla::CALLE && !no_tiene_propietario)
-        comprar = @@vista.elegir_quiero_comprar
+    def casilla_tipo_sorpresa(no_tiene_propietario)
+      @vista.mostrar('Ha caido en la carta sorpresa\n')
+      @vista.mostrar("La carte que le ha tocado es: " + @juego.carta_actual.to_s() + "\n")
+      no_tiene_propietario = @juego.aplicar_sorpresa
+      @casilla = @jugador.casilla_actual
+      if( !@jugador.encarcelado && !bancarrota &&@casilla.tipo == ModeloQytetet::TipoCasilla::CALLE && !no_tiene_propietario)
+        comprar = @vista.elegir_quiero_comprar
         if(comprar)
-          if(@@juego.comprar_titulo_propiedad)
-            @@vista.mostrar("Se ha comprado la propiedad");                            
+          if(@juego.comprar_titulo_propiedad)
+            @vista.mostrar("Se ha comprado la propiedad");                            
           else
-            @@vista.mostrar("No se ha comprado la propiedad");                            
+            @vista.mostrar("No se ha comprado la propiedad");                            
           end
         end
       end
     end
     
-    def self.bancarrota
-      return @@jugador.saldo <= 0
+    def bancarrota
+      return @jugador.saldo <= 0
     end
     
-    def self.esta_encarcelado
-      @@vista.mostrar("Esta en la casilla carcel")
+    def esta_encarcelado
+      @vista.mostrar("Esta en la casilla carcel")
       libre = false
-      opcion_carcel = @@vista.menu_salir_carcel
+      opcion_carcel = @vista.menu_salir_carcel
       if( opcion_carcel == 1)
-        libre = @@juego.intentar_salir_carcel(ModeloQytetet::MetodoSalirCarcel::PAGANDOLIBERTAD)
+        libre = @juego.intentar_salir_carcel(ModeloQytetet::MetodoSalirCarcel::PAGANDOLIBERTAD)
       else
-        libre = @@juego.intentar_salir_carcel(ModeloQytetet::MetodoSalirCarcel::TIRANDODADO)
+        libre = @juego.intentar_salir_carcel(ModeloQytetet::MetodoSalirCarcel::TIRANDODADO)
       end
       if(libre)
-        @@vista.mostrar("Has sido liberado de la carcel")
+        @vista.mostrar("Has sido liberado de la carcel")
       end
       return libre
     end
     
-    def self.esta_libre
-      @@vista.pausa
-      no_tiene_propietario = @@juego.jugar
-      num_dado = @@jugador.casilla_actual.numeroCasilla - @@casilla.numeroCasilla
+    def esta_libre
+      @vista.pausa
+      no_tiene_propietario = @juego.jugar
+      num_dado = @jugador.casilla_actual.numeroCasilla - @casilla.numeroCasilla
       if(num_dado < 0)
         num_dado = num_dado + 20
       end
-      if(!@@jugador.encarcelado)
-        @@vista.mostrar("Saco un " + num_dado.to_s + " al tirar el dado")
+      if(!@jugador.encarcelado)
+        @vista.mostrar("Saco un " + num_dado.to_s + " al tirar el dado")
       end
-      @@casilla = @@jugador.casilla_actual
+      @casilla = @jugador.casilla_actual
             
       if(!bancarrota)
-        if(!@@jugador.encarcelado)
+        if(!@jugador.encarcelado)
           
-          if(@@casilla.tipo == ModeloQytetet::TipoCasilla::CALLE)
+          if(@casilla.tipo == ModeloQytetet::TipoCasilla::CALLE)
             casilla_tipo_calle(no_tiene_propietario)
           end
 
-          if(@@casilla.tipo == ModeloQytetet::TipoCasilla::SORPRESA)
+          if(@casilla.tipo == ModeloQytetet::TipoCasilla::SORPRESA)
             casilla_tipo_sorpresa(no_tiene_propietario)
           end
               
-          if(!@@jugador.encarcelado && !bancarrota() && @@jugador.tengo_propiedades)
+          if(!@jugador.encarcelado && !bancarrota() && @jugador.tengo_propiedades)
             opcion = 1
-            while(opcion !=0 && @@jugador.tengo_propiedades)
-              @@vista.mostrar("\nPropiedades del jugador")
-              @@jugador.propiedades.each do |p|
-                @@vista.mostrar("Nombre: "+ p.nombre + " Numero de casas: " + p.casilla.numCasas.to_s+ " Numero de hoteles: " + p.casilla.numHoteles.to_s + " Hipotecada: " + p.hipotecada.to_s)
-                @@vista.mostrar("")
+            while(opcion !=0 && @jugador.tengo_propiedades)
+              @vista.mostrar("\nPropiedades del jugador")
+              @jugador.propiedades.each do |p|
+                @vista.mostrar("Nombre: "+ p.nombre + " Numero de casas: " + p.casilla.numCasas.to_s+ " Numero de hoteles: " + p.casilla.numHoteles.to_s + " Hipotecada: " + p.hipotecada.to_s)
+                @vista.mostrar("")
               end
-              opcion = @@vista.menu_gestion_inmobiliaria
+              opcion = @vista.menu_gestion_inmobiliaria
                   
               gestion_inmobiliarioa(opcion)
                  
             end
           end
-          if (!@@jugador.tengo_propiedades && !@@jugador.encarcelado && !bancarrota()) 
-            @@vista.mostrar("Usted no tiene propiedades para gestionar\n");
+          if (!@jugador.tengo_propiedades && !@jugador.encarcelado && !bancarrota()) 
+            @vista.mostrar("Usted no tiene propiedades para gestionar\n");
           end
         else
-          @@vista.mostrar("El jugador ha sido encarcelado")
+          @vista.mostrar("El jugador ha sido encarcelado")
         end
       end
     end
     
-    def self.desarrollo_juego
+    def desarrollo_juego
       while(!bancarrota)
-        @@vista.mostrar("Turno de: "+@@jugador.nombre)
-        @@vista.mostrar("Saldo de: "+@@jugador.saldo.to_s)        
-        @@vista.mostrar("Casilla actual numero: " + @@casilla.numeroCasilla.to_s)
+        @vista.mostrar("Turno de: "+@jugador.nombre)
+        @vista.mostrar("Saldo de: "+@jugador.saldo.to_s)        
+        @vista.mostrar("Casilla actual numero: " + @casilla.numeroCasilla.to_s)
         libre = true
-        if(@@jugador.encarcelado)
+        if(@jugador.encarcelado)
           libre = esta_encarcelado
         end
 
@@ -196,28 +198,29 @@ module InterfazTextualQytetet
           esta_libre
         end
         if (!bancarrota()) 
-          @@jugador = @@juego.siguiente_jugador
-          @@casilla = @@jugador.casilla_actual
+          @jugador = @juego.siguiente_jugador
+          @casilla = @jugador.casilla_actual
         end       
       end
-      @@vista.mostrar(@@juego.obtener_ranking)
+      @vista.mostrar(@juego.obtener_ranking)
     end
     ## --------------------el siguiente método va en ControladorQytetet
-    def self.elegir_propiedad(propiedades) # lista de propiedades a elegir
-      @@vista.mostrar("\tCasilla\tTitulo");
+    def elegir_propiedad(propiedades) # lista de propiedades a elegir
+      @vista.mostrar("\tCasilla\tTitulo");
         
       listaPropiedades= Array.new
       for prop in propiedades  # crea una lista de strings con numeros y nombres de propiedades
         propString= prop.numeroCasilla.to_s+' '+prop.titulo.nombre; 
         listaPropiedades<<propString
       end
-      seleccion=@@vista.menu_elegir_propiedad(listaPropiedades)  # elige de esa lista del menu
+      seleccion=@vista.menu_elegir_propiedad(listaPropiedades)  # elige de esa lista del menu
       propiedades.at(seleccion)
     end
     
     def self.main
-      self.incializar_juego
-      self.desarrollo_juego
+      controlador = Controlador_qytetet.new
+      controlador.incializar_juego
+      controlador.desarrollo_juego
     end
     
     Controlador_qytetet.main
